b04ceb86fd5ead9e953abdacc302bfbfdbe0fd4f
ACampero <andresmiamifan@hotmail.com>
2017-10-16 11:03:14 -0400

    genlex

--- scripts/ccgAndres.py
+++ scripts/ccgAndres.py
@@ -15,21 +15,25 @@ lex2 = lexicon.fromstring(r"""
     DET :: NN/NN
     ADJ :: NN/NN
     IN :: (NN\NN)/NN
-    ADV :: NN/NN
+    ADV :: NN/ADJ
     QS :: NN/NN
 
     same => ADJ {\x.same_(x)}
+
+    same => IN {\x y.same_(x,y)}
+
 
     material => NN {'material'}
     color => NN {'color'}
     shape => NN {'shape'}
     size => NN {'size'}
 
-    as => IN {\x.(x)}
-    of => IN {\x y.pair(x,y)}
-    to => IN {\x.relate(x)}
+    as => IN {\x y.as(x,y)}
+    of => IN {\x y.of(x,y)}
 
-    the => DET {\P.unique(P)}
+    to => DET {\x.relate(x)}
+
+    the => DET {\P.(P)}
     a => DET {\P.(P)}
     an => DET {\P.(P)}
     any => DET {\P.(P)}
@@ -57,17 +61,11 @@ lex2 = lexicon.fromstring(r"""
 
     left => ADJ {\x.left(x,'prueba')}
     left => NN {'left'}
-    right => ADJ {\x.right(x,'right')}
-    right => NN {'right'}
-    front => ADJ {\x.front(x)}
-    front => NN {'front'}
-    behind => ADV {\x.filter_size(x,'behind')}
-    behind => NN {'behind'}
-
 
     cube => NN {'cube'}
     block => NN {'cube'}
-    sphere => NN {'sphere'}
+    sphere => ADJ {\x.filter_shape(x,'sphere')}
+    spheres => NN {'sphere'}
     ball => NN {'sphere'}
     cylinder => NN {'cylinder'}
 
@@ -91,7 +89,53 @@ lex2 = lexicon.fromstring(r"""
     #Is the purple thing the same shape as the large gray rubber thing?
     #equal_shape(query_shape(unique(filter_color(scene,u'purple'))),query_shape(unique(filter_material(filter_color(filter_size(scene,u'large'),u'gray'),u'rubber'))))
 parser = chart.CCGChartParser(lex2, chart.DefaultRuleSet)
-results = list(parser.parse("the same shape as the big metallic object".split()))
-chart.printCCGDerivation(results[-1])
 
-    #are there any other things that are => S {\x.exist(x)} 
+#results = list(parser.parse("the same shape as the big metallic object".split()))
+#results = list(parser.parse("a big brown object of the same shape as the green thing".split()))
+results = list(parser.parse("the material of the big purple object".split()))
+#results = list(parser.parse("any sphere to the left of it".split()))
+#results = list(parser.parse("the purple thing the same shape as the large gray rubber thing".split()))
+
+chart.printCCGDerivation(results[0])
+
+
+#are there any other things that are => S {\x.exist(x)} 
+#right => ADJ {\x.right(x,'right')}
+#right => NN {'right'}
+#front => ADJ {\x.front(x)}
+#front => NN {'front'}
+#behind => ADV {\x.filter_size(x,'behind')}
+#behind => NN {'behind'}
+
+
+##Inference
+semantics = True
+lex_inference = lexicon.fromstring(r"""
+    :- NN, INP, ADJ, DET, IN
+
+    DET :: NN/NN
+    ADJ :: NN/NN
+    IN :: (NN\NN)/NN
+    ADV :: NN/ADJ
+    QS :: NN/NN
+
+
+    the => DET {\P.(P)}
+    the => DET {\P.prueba(P)}
+    big => ADJ {\x.filter_size(x,'large')}
+    object => NN {scene}
+   
+    """, include_semantics=semantics)
+
+print(lex_inference.__string__())
+parser = chart.CCGChartParser(lex_inference, chart.DefaultRuleSet)
+
+sentence = "the purple big object"
+new_lex = parser.genlex(sentence) 
+print(new_lex.__string__())
+
+
+
+
+
+ 
