667bebf4e2f13492e9f51e044cd1d666ca025ce0
Jon Gauthier <jon@gauthiers.net>
2017-10-18 17:18:32 -0400

    slightly more complicated example

--- scripts/learn.py
+++ scripts/learn.py
@@ -8,27 +8,6 @@ from nltk.ccg import chart, lexicon
 import numpy as np
 
 
-class WeightedCCGLexicon(lexicon.CCGLexicon):
-    """
-    A CCG lexicon which maintains a decoupled efficient representation of the
-    weights of its tokens.
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self._weights = np.zeros(len(self._entries))
-        # for i, entry in enumerate(self._entries):
-        #     self._weights[i] = entry.weight()
-        self._weights = {entry: token.weight()
-                         for entry, token in self._entries.items()}
-
-        print(self._weights)
-
-
-question = "the pen"
-parse = "unique(utensil)"
-
 semantics = True
 
 lex = lexicon.fromstring(r"""
@@ -38,21 +17,24 @@ lex = lexicon.fromstring(r"""
 
     the => DET {\x.unique(x)}
 
-    pen => NN {utensil}
-    pen => NN {area}
+    blue => NN {red}
+    blue => NN {blue}
+
+    red => NN {red}
+    red => NN {blue}
     """, include_semantics=semantics)
 
-parser = chart.CCGChartParser(lex, chart.DefaultRuleSet)
-results = parser.parse(question.split())
 
-for result in results:
-    chart.printCCGDerivation(result)
-    token, _ = result.label()
-    print(token.semantics())
+data = [
+    ("the blue".split(), "unique(blue)"),
+    ("the red".split(), "unique(red)"),
+]
 
 
 def learn(lexicon, data):
-    learning_rate = 0.5
+    parser = chart.CCGChartParser(lex, chart.DefaultRuleSet)
+
+    learning_rate = 0.1
     for x, y in data:
         weighted_results = parser.parse(x, return_weights=True)
 
@@ -69,6 +51,5 @@ def learn(lexicon, data):
                 leaf_token._weight += sign * 1
 
 
-learn(lex, [("the pen".split(), "unique(utensil)")])
-print()
-learn(lex, [("the pen".split(), "unique(utensil)")])
+for _ in range(3):
+    learn(lex, data)
