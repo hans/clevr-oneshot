645618445f8dbab3095b6aeb346ee022b30f496c
Jon Gauthier <jon@gauthiers.net>
2017-11-03 09:52:26 -0400

    demonstrate learning of a spatial relation!

--- scripts/learn_with_genlex.py
+++ scripts/learn_with_genlex.py
@@ -31,7 +31,7 @@ data_phase1 = [
 ]
 
 data_phase2 = [
-  ("the blue ball".split(), "unique(color(ball,blue))"),
+  ("above the blue ball".split(), "at(unique(color(ball,blue)),above)"),
 ]
 
 # Strengthen weights for phase 1.
@@ -41,6 +41,7 @@ update_perceptron_batch(lex, data_phase1)
 lex_aug = augment_lexicon(lex, data_phase2[0][0], data_phase2[0][1])
 #lex_aug = filter_lexicon_entry(lex_aug, "blue", data_phase2[0][0], data_phase2[0][1])
 lex_aug = update_weights_rsa(lex_aug, "blue")
+lex_aug = update_weights_rsa(lex_aug, "above")
 print(lex_aug)
 
 # Strengthen weights for phase 2.
