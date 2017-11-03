36acfaf9130224490717d53fcbc3b6027a67b4f7
Jon Gauthier <jon@gauthiers.net>
2017-11-03 09:52:16 -0400

    improve perceptron algorithm

--- clevros/perceptron.py
+++ clevros/perceptron.py
@@ -25,15 +25,23 @@ def update_perceptron_batch(lexicon, data, learning_rate=0.1, parser=None):
   norm = 0.0
   for x, y in data:
     weighted_results = parser.parse(x, return_aux=True)
+
+    max_result, max_score, _ = weighted_results[0]
+    correct_result, correct_score = None, None
+
     for result, score, _ in weighted_results:
       root_token, _ = result.label()
-      correct = str(root_token.semantics()) == y
-      sign = 1 if correct else -1
+      if str(root_token.semantics()) == y:
+        correct_result, correct_score = result, score
+        break
+    else:
+      raise ValueError("no valid parse derived")
 
-      for _, leaf_token in result.pos():
-        delta = sign * learning_rate
-        norm += delta ** 2
-
-        leaf_token._weight += delta
+    if correct_score < max_score:
+      for result, sign in zip([correct_result, max_result], [1, -1]):
+        for _, leaf_token in result.pos():
+          delta = sign * learning_rate
+          norm += delta ** 2
+          leaf_token._weight += delta
 
   return norm
