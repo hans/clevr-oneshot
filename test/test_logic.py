b9160bd1024169f03093fcc90e0ebbc835d9ad70
Jon Gauthier <jon@gauthiers.net>
2018-04-23 14:07:00 -0400

    add function to convert S-expression -> expr

--- test/test_logic.py
+++ test/test_logic.py
@@ -8,3 +8,7 @@ from clevros.logic import *
 def test_as_ec_sexpr():
   expr = Expression.fromstring(r"\x y z.foo(bar(x,y),baz(y,z),blah)")
   eq_(as_ec_sexpr(expr), "(lambda (lambda (lambda (foo (bar $0 $1) (baz $1 $2) blah))))")
+
+
+def test_read_ec_sexpr():
+  expr = read_ec_sexpr("(lambda (lambda (lambda (foo (bar $0 $1) (baz $1 $2) blah))))")
