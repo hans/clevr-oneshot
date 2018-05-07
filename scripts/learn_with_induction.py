e5a178beee6b9c4560b22c0a93311f8dd979bdfd
Jon Gauthier <jon@gauthiers.net>
2018-05-07 16:28:21 -0400

    EC update + hyperparam settings which make the proper invention show up! yay!

--- scripts/learn_with_induction.py
+++ scripts/learn_with_induction.py
@@ -26,9 +26,9 @@ random.seed(4)
 EC_kwargs = {
   "topK": 1,
   "pseudoCounts": 1.0,
-  "a": 2,
+  "a": 1,
   "aic": -1.0,
-  "structurePenalty": 0,
+  "structurePenalty": 0.001,
   "backend": "pypy",
   "CPUs": 1,
 }
